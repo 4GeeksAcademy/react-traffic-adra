c1bd72789044fd0eca794b7ee727b923c1ce8a75
kinnetik0 <alexis.rosquete@gmail.com>
2024-06-12 22:15:44 +0200

    Update -Color Purple

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 const TrafficLight = () => {
     const [selectedLight, setSelectedLight] = useState("");
     const [active, setActive] = useState(false);
+    const [purple, setPurple] = useState(false);
 
     const handleLightClick = (color) => {
         setSelectedLight(color);
@@ -19,10 +20,13 @@ const TrafficLight = () => {
                 } else if (selectedLight == "yellow") {
                     setSelectedLight("green");
                 } else if (selectedLight == "green") {
+                    setSelectedLight("purple");
+                } else if (selectedLight == "purple") {
                     setSelectedLight("red");
                 }
-            }, 3000);
+            }, 1000);
         }
+
         return () => clearInterval(interval);
     }, [active, selectedLight]);
 
@@ -46,12 +50,27 @@ const TrafficLight = () => {
                     }`}
                     onClick={() => handleLightClick("green")}
                 ></div>
+                {purple ? (
+                    <div
+                        className={`purple ${
+                            selectedLight == "purple" ? "light" : ""
+                        }`}
+                        onClick={() => handleLightClick("purple")}
+                    ></div>
+                ) : null}
+                {/* {purple ? <div className="purple" /> : null} */}
             </div>
             <button
                 onClick={() => setActive((prev) => !prev)}
                 className="btn btn-dark"
             >
                 Automatic
+            </button>
+            <button
+                onClick={() => setPurple((prev) => !prev)}
+                className="btn btn-dark"
+            >
+                {purple ? "Remove Purple" : "Add Purple"}
             </button>
         </main>
     );
